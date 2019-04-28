11605d91e8bfa557e67abc5685705dd6723e6538
honsu001 <43789499+honsu001@users.noreply.github.com>
2019-04-29 03:40:48 +0800

    Add files via upload

--- evil-extension/page.js
+++ evil-extension/page.js
@@ -10,11 +10,33 @@ const MATCH_LIST = {
   'THEY\'RE': 'THERE'
 };
 
-function transformTextNodes(node) {
+var new_text = "";
+
+function transformTextNodes(node) { 
   // TODO(you): Implement this function! See HW spec for details.
+	if(node.nodeType === Node.TEXT_NODE && node.nodeName !== '<script>' && node.nodeName !== '<style>'){
+		var text = node.textContent.trim();
+		var words = text.split(/\s+|\n+/);
+	  
+		for(var i = 0;i < words.length;i++){
+			if (MATCH_LIST[words[i]]){
+				words[i] = MATCH_LIST[words[i]];
+			}
+			new_text += words[i] + " ";
+			node.textContent = new_text;
+		}
+		new_text = "";
+	} 
+	else{
+		for(const child of node.childNodes){
+			transformTextNodes(child);
+		}
+	}
 }
 
 transformTextNodes(document.body);
 
 // Log statement to test that the extension loaded properly.
 console.log('Evil extension loaded!');
+
+console.log('Extension updated');
